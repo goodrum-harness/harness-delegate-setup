c132e438b6352433eaad028f9d95e76c87546234
Jeremy Goodrum <jeremy.goodrum@harness.io>
2024-02-12 12:49:22 -0600

    ADD: support for go-template

---
+++ docker/scripts/install_go-template.sh
@@ -0,0 +1,4 @@
+mkdir -p /opt/harness-delegate/client-tools/go-template/v0.4.1/
+curl -L0 https://app.harness.io/public/shared/tools/go-template/release/v0.4.1/bin/linux/amd64/go-template -o go-template
+chmod +x ./go-template
+mv go-template /usr/local/bin/
