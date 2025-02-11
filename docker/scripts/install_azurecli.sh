c044ab4fd76024c0f4f1567b6064f91ba5d918e4
Jeremy Goodrum <jeremy.goodrum@harness.io>
2025-02-11 10:38:04 -0800

    UPDATE: Scripts::InstallAzureCli to use RHEL9 version

--- docker/scripts/install_azurecli.sh
+++ docker/scripts/install_azurecli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "-> Downloading Package"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-rpm -Uvh 'https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm'
+rpm -Uvh 'https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm'
 microdnf install azure-cli
 echo "-> Installation Complete"
 az --version
